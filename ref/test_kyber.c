d73fb3cb316372ba5a36a417f8a7fed2fd29a5bf
dinesh karthikeyan0711 <dineshkarthikeyan0711@gmail.com>
2023-08-14 17:13:44 +0100

    Verification of resp at each instantce and printing the params

--- ref/test_kyber.c
+++ ref/test_kyber.c
@@ -8,108 +8,141 @@
 
 static int test_keys()
 {
-  uint8_t pk[CRYPTO_PUBLICKEYBYTES];
-  uint8_t sk[CRYPTO_SECRETKEYBYTES];
-  uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
-  uint8_t key_a[CRYPTO_BYTES];
-  uint8_t key_b[CRYPTO_BYTES];
+    uint8_t pk[CRYPTO_PUBLICKEYBYTES];
+    uint8_t sk[CRYPTO_SECRETKEYBYTES];
+    uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
+    uint8_t key_a[CRYPTO_BYTES];
+    uint8_t key_b[CRYPTO_BYTES];
 
-  //Alice generates a public key
-  crypto_kem_keypair(pk, sk);
+    // Alice generates a public key
+    crypto_kem_keypair(pk, sk);
 
-  //Bob derives a secret key and creates a response
-  crypto_kem_enc(ct, key_b, pk);
+    // Bob derives a secret key and creates a response
+    crypto_kem_enc(ct, key_b, pk);
 
-  //Alice uses Bobs response to get her shared key
-  crypto_kem_dec(key_a, ct, sk);
+    // Alice uses Bob's response to get her shared key
+    crypto_kem_dec(key_a, ct, sk);
 
-  if(memcmp(key_a, key_b, CRYPTO_BYTES)) {
-    printf("ERROR keys\n");
-    return 1;
-  }
+    if (memcmp(key_a, key_b, CRYPTO_BYTES)) {
+        printf("ERROR keys: Test failed\n");
+        printf("key_a: ");
+        for (size_t i = 0; i < CRYPTO_BYTES; i++) {
+            printf("%02X", key_a[i]);
+        }
+        printf("\nkey_b: ");
+        for (size_t i = 0; i < CRYPTO_BYTES; i++) {
+            printf("%02X", key_b[i]);
+        }
+        return 1;
+    } else {
+        printf("Keys test successful\n");
+    }
 
-  return 0;
+    return 0;
 }
 
 static int test_invalid_sk_a()
 {
-  uint8_t pk[CRYPTO_PUBLICKEYBYTES];
-  uint8_t sk[CRYPTO_SECRETKEYBYTES];
-  uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
-  uint8_t key_a[CRYPTO_BYTES];
-  uint8_t key_b[CRYPTO_BYTES];
+    uint8_t pk[CRYPTO_PUBLICKEYBYTES];
+    uint8_t sk[CRYPTO_SECRETKEYBYTES];
+    uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
+    uint8_t key_a[CRYPTO_BYTES];
+    uint8_t key_b[CRYPTO_BYTES];
 
-  //Alice generates a public key
-  crypto_kem_keypair(pk, sk);
+    // Alice generates a public key
+    crypto_kem_keypair(pk, sk);
 
-  //Bob derives a secret key and creates a response
-  crypto_kem_enc(ct, key_b, pk);
+    // Bob derives a secret key and creates a response
+    crypto_kem_enc(ct, key_b, pk);
 
-  //Replace secret key with random values
-  randombytes(sk, CRYPTO_SECRETKEYBYTES);
+    // Replace secret key with random values
+    randombytes(sk, CRYPTO_SECRETKEYBYTES);
 
-  //Alice uses Bobs response to get her shared key
-  crypto_kem_dec(key_a, ct, sk);
+    // Alice uses Bob's response to get her shared key
+    crypto_kem_dec(key_a, ct, sk);
 
-  if(!memcmp(key_a, key_b, CRYPTO_BYTES)) {
-    printf("ERROR invalid sk\n");
-    return 1;
-  }
+    if (!memcmp(key_a, key_b, CRYPTO_BYTES)) {
+        printf("ERROR invalid sk: Test failed\n");
+        printf("key_a: ");
+        for (size_t i = 0; i < CRYPTO_BYTES; i++) {
+            printf("%02X", key_a[i]);
+        }
+        printf("\nkey_b: ");
+        for (size_t i = 0; i < CRYPTO_BYTES; i++) {
+            printf("%02X", key_b[i]);
+        }
+        return 1;
+    } else {
+        printf("Invalid secret key test successful\n");
+    }
 
-  return 0;
+    return 0;
 }
 
 static int test_invalid_ciphertext()
 {
-  uint8_t pk[CRYPTO_PUBLICKEYBYTES];
-  uint8_t sk[CRYPTO_SECRETKEYBYTES];
-  uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
-  uint8_t key_a[CRYPTO_BYTES];
-  uint8_t key_b[CRYPTO_BYTES];
-  uint8_t b;
-  size_t pos;
+    uint8_t pk[CRYPTO_PUBLICKEYBYTES];
+    uint8_t sk[CRYPTO_SECRETKEYBYTES];
+    uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
+    uint8_t key_a[CRYPTO_BYTES];
+    uint8_t key_b[CRYPTO_BYTES];
+    uint8_t b;
+    size_t pos;
 
-  do {
-    randombytes(&b, sizeof(uint8_t));
-  } while(!b);
-  randombytes((uint8_t *)&pos, sizeof(size_t));
+    do {
+        randombytes(&b, sizeof(uint8_t));
+    } while (!b);
+    randombytes((uint8_t *)&pos, sizeof(size_t));
 
-  //Alice generates a public key
-  crypto_kem_keypair(pk, sk);
+    // Alice generates a public key
+    crypto_kem_keypair(pk, sk);
 
-  //Bob derives a secret key and creates a response
-  crypto_kem_enc(ct, key_b, pk);
+    // Bob derives a secret key and creates a response
+    crypto_kem_enc(ct, key_b, pk);
 
-  //Change some byte in the ciphertext (i.e., encapsulated key)
-  ct[pos % CRYPTO_CIPHERTEXTBYTES] ^= b;
+    // Change some byte in the ciphertext (i.e., encapsulated key)
+    ct[pos % CRYPTO_CIPHERTEXTBYTES] ^= b;
 
-  //Alice uses Bobs response to get her shared key
-  crypto_kem_dec(key_a, ct, sk);
+    // Alice uses Bob's response to get her shared key
+    crypto_kem_dec(key_a, ct, sk);
 
-  if(!memcmp(key_a, key_b, CRYPTO_BYTES)) {
-    printf("ERROR invalid ciphertext\n");
-    return 1;
-  }
+    if (!memcmp(key_a, key_b, CRYPTO_BYTES)) {
+        printf("ERROR invalid ciphertext: Test failed\n");
+        printf("key_a: ");
+        for (size_t i = 0; i < CRYPTO_BYTES; i++) {
+            printf("%02X", key_a[i]);
+        }
+        printf("\nkey_b: ");
+        for (size_t i = 0; i < CRYPTO_BYTES; i++) {
+            printf("%02X", key_b[i]);
+        }
+        return 1;
+    } else {
+        printf("Invalid ciphertext test successful\n");
+    }
 
-  return 0;
+    return 0;
 }
 
 int main(void)
 {
-  unsigned int i;
-  int r;
+    unsigned int i;
+    int r;
 
-  for(i=0;i<NTESTS;i++) {
-    r  = test_keys();
-    r |= test_invalid_sk_a();
-    r |= test_invalid_ciphertext();
-    if(r)
-      return 1;
-  }
+    for (i = 0; i < NTESTS; i++) {
+        r = test_keys();
+        r |= test_invalid_sk_a();
+        r |= test_invalid_ciphertext();
+        if (r) {
+            printf("Overall test iteration %d failed\n", i + 1);
+            return 1;
+        }
+    }
 
-  printf("CRYPTO_SECRETKEYBYTES:  %d\n",CRYPTO_SECRETKEYBYTES);
-  printf("CRYPTO_PUBLICKEYBYTES:  %d\n",CRYPTO_PUBLICKEYBYTES);
-  printf("CRYPTO_CIPHERTEXTBYTES: %d\n",CRYPTO_CIPHERTEXTBYTES);
+    printf("All tests passed successfully\n");
+    printf("CRYPTO_SECRETKEYBYTES:  %d\n", CRYPTO_SECRETKEYBYTES);
+    printf("CRYPTO_PUBLICKEYBYTES:  %d\n", CRYPTO_PUBLICKEYBYTES);
+    printf("CRYPTO_CIPHERTEXTBYTES: %d\n", CRYPTO_CIPHERTEXTBYTES);
 
-  return 0;
+    return 0;
 }
